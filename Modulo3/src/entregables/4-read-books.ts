445fe0dfaecf57f97c2174fc7509556f13b04c20
Ruben <rzubicoa@eservicios.indra.es>
2026-01-30 17:40:54 +0100

    feat(Modulo3): realizar la parte de read books de los entregables

---
+++ Modulo3/src/entregables/4-read-books.ts
@@ -0,0 +1,19 @@
+// Read Books
+// Crea una función isBookRead que reciba una lista de libros y un título y devuelva si se ha leído o no dicho libro. 
+// Un libro es un objeto con title como string y isRead como booleano. En caso de no existir el libro devolver false TIP: 
+// Existe un método de Array.prototype que te ayudará a buscar según un patrón.
+
+interface Book {title: string, isRead: boolean}
+const books = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+function isBookRead(books: Book[], titleToSearch: Book['title']): boolean{
+    return books.find((book) => book.title === titleToSearch)?.isRead ?? false;
+}
+
+console.log(isBookRead(books, "Devastación"));
+console.log(isBookRead(books, "Canción de hielo y fuego"));
+console.log(isBookRead(books, "Los Pilares de la Tierra")); 
