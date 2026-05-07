ddbf4a4651a9326918bb094c1d0c58128c5796ec
Zubicoa Garijo <rzubicoa@minsait.com>
2026-05-07 12:04:53 +0200

    feat: crear el formulario del login

--- Modulo5/Angular/laboratorio/src/app/login/pages/login/login.ts
+++ Modulo5/Angular/laboratorio/src/app/login/pages/login/login.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
