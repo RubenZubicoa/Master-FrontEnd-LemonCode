cf6d1dd6155cfe98c11c4f74f3e96fa9bc1bc058
Zubicoa Garijo <rzubicoa@minsait.com>
2026-05-12 11:35:49 +0200

    feat: girar la imagen al lado contrario

--- Modulo5/Angular/laboratorio/src/app/galeria/directives/rotate.ts
+++ Modulo5/Angular/laboratorio/src/app/galeria/directives/rotate.ts
@@ -1,20 +1,21 @@
-import { Directive, ElementRef, HostBinding, HostListener, inject, Input, input, OnChanges, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: 'img[appRotate]',
 })
-export class Rotate implements OnChanges {
+export class Rotate implements OnInit {
   @Input() defaultRotate: number = 0;
 
   private elementRef: ElementRef = inject(ElementRef);
   private deg = 0;
 
-  @HostListener('click')
-  onClick() {
-    this.rotateImage();
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    const step = 10;
+    this.rotateImage(event.shiftKey ? -step : step);
   }
 
-  ngOnChanges(): void {
+  ngOnInit(): void {
     if(this.defaultRotate) {
       this.rotateImage(this.defaultRotate);
     }
